fad44ffe5c421ba1655e8d2e867334bcda066fc0
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-04-18 10:16:21 -0300

    Aula File FileInfo e IOException

---
+++ AulaFileFileInfoEIOException/AulaFileFileInfoEIOException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaFileFileInfoEIOException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sourcePath = @"C:\temp\file1.txt";
+            string targetPath = @"C:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
