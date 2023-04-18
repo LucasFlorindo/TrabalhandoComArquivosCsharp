c28f1bd575dd08edf57ffa5126842f1c6503986f
Lucas Florindo <lucasflorindoeg@gmail.com>
2023-04-18 10:40:59 -0300

    FileStream e StreamReader

---
+++ FileStreamEStreamReader/FileStreamEStreamReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamEStreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\file1.txt";
+        
+            StreamReader sr = null;
+
+            try
+            {
+                
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+               
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(sr != null)
+                {
+                    sr.Close();
+                }
+            
+            }
+
+        }
+    }
+}
